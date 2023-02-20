46948efc8c49945a1af71d6bdff5a14fa866d546
Henry Le <henryle913@outlook.com>
2023-02-20 14:44:18 -0600

    Adding animation & functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import logo from './img/cats_clicker.gif';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [numClicked, setNumClicked] = useState(0);
 
   const handleButtonClick = () => {
     console.log('button clicked');
+    setNumClicked(numClicked+1);
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <h2 className='numberClickStyle'>{numClicked}</h2>
         <button className='mainButton' onClick={handleButtonClick}><img src={logo} className="App-logo" alt="logo" /></button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
