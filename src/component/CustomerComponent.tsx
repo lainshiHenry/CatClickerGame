19320abafda7e76f6262b880068a0bc017d79ad7
Henry Le <henryle913@outlook.com>
2023-03-12 09:44:36 -0500

    Implementing Customer and customer buying action

--- src/component/CustomerComponent.tsx
+++ src/component/CustomerComponent.tsx
@@ -6,11 +6,10 @@ import { listOfMakableItemsNames } from '../data/ListOfEnum'
 
 const CustomerComponent = ({customer, handleCustomerPurchase}: {customer: Customer, handleCustomerPurchase: (itemToSell: listOfMakableItemsNames) => void}) => {
 
-
   return (
     <div className='customerComponent'>
-        <p>I want to buy {customer.getItemToBuy.getNameOfItem}</p>
-        <button className='mainButton' onClick={() => {handleCustomerPurchase(customer.getItemToBuy.getNameOfItem)}}>
+        <p>I want to buy {customer.getItemToBuy?.getNameOfItem}</p>
+        <button className='mainButton' onClick={() => {handleCustomerPurchase(customer.getItemToBuy!.getNameOfItem)}}>
             <img src={customer.getImgPath} className='App-logo'></img>
         </button>
         
