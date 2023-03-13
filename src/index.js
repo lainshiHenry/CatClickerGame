7c542b2d3e13c98beafac2012af56045af179932
Henry Le <henryle913@outlook.com>
2023-03-13 14:07:20 -0500

    Installing Google Analytics

--- src/index.js
+++ src/index.js
@@ -4,17 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration.js'
+import ReactGA from 'react-ga4';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//initialize GA4
+ReactGA.initialize("G-4CNCZY59K7");
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
 
 serviceWorkerRegistration.register();
